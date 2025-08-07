f73dbac16618e7bff168fe6d52dc7efc5e4c4e3c
tantantanxinyi <1264030852@qq.com>
2025-08-07 20:08:20 +0800

    modify the Header component and adjust the scroll event handling logic, ensuring the header displays and hides correctly during scrolling.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased `}>
+      <body className={`${inter.className} antialiased min-h-[125vh]`}>
         <Header></Header>
-        {children}</body>
+        {children}
+      </body>
     </html>
   );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 
 const AnnouncementBar = () => {
@@ -13,24 +14,40 @@ const AnnouncementBar = () => {
 };
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
   const [prevScrollY, setPrevScrollY] = useState<number>(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
+      console.log(currentScrollY);
+      console.log(prevScrollY, "22222");
       const scrolledUp = currentScrollY < prevScrollY;
+
+      if (scrolledUp) {
+        setIsOpen(true);
+      } else if (currentScrollY > 100) {
+        setIsOpen(false);
+      }
+
+      setPrevScrollY(currentScrollY); // 更新上一次滚动位置
     };
 
-    
+    setPrevScrollY(window.scrollY); // 初始化滚动位置
+    window.addEventListener("scroll", handleScroll);
 
-
-    setPrevScrollY(window.scrollY);
-  }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollY]);
 
   return (
     <header className="w-full sticky top-0 z-50 bg-white">
-      <div className="w-full transform transition-all duration-300 ease-in-out">
+      <div
+        className={`w-full transform transition-all duration-300 ease-in-out ${
+          isOpen ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
         <AnnouncementBar></AnnouncementBar>
       </div>
     </header>
