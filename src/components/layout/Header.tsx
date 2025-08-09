d7d6fce8d3b9fca3731d9efeda5e74a8dc077c1f
tantantanxinyi <1264030852@qq.com>
2025-08-09 09:40:59 +0800

    Update dependencies, add support for Lucia authentication, modify Prisma models to support users and sessions.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 const AnnouncementBar = () => {
@@ -49,6 +50,76 @@ const Header = () => {
         }`}
       >
         <AnnouncementBar></AnnouncementBar>
+        <div className="w-full flex justify-between items-center py-3 sm:py-4 bg-white shadow-sm border-b border-gray-100 backdrop-blur-sm">
+          <div className="flex justify-between items-center container mx-auto px-8">
+            <div className="flex flex-1 justify-start items-center gap-4 sm:gap-6">
+              <button className="text-gray-700 hover:text-gray-900 md:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 sm:h-6 sm:w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+              <nav className="hidden md:flex gap-4 lg:gap-6 text-sm font-medium">
+                <Link href="#">Shop</Link>
+                <Link href="#">New Arrivals</Link>
+                <Link href="#">Sale</Link>
+              </nav>
+            </div>
+            <Link href="#"> link </Link>
+
+            <div className="flex flex-1 justify-end items-center gap-2 sm:gap-4">
+              <button className="text-gray-700 hover:text-gray-900 hidden  sm:block">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 sm:h-6 sm:w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+
+              <Link href="/auth/sign-in">Sign In</Link>
+              <Link href="/auth/sign-up">Sign Up</Link>
+
+              <button className="text-gray-700 hover:text-gray-900 relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 sm:h-6 sm:w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                  />
+                </svg>
+                <span className="absolute -top-1 -right-1 bg-black text-white text-[10px] sm:text-xs w-3.5 h-3.5 sm:h-4 sm:w-4 rounded-full flex items-center justify-center ">
+                  0
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );
