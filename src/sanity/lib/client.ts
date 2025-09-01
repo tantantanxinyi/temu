b4590241f169f27ee60a3384bc378bf555eeb97a
tantantanxinyi <1264030852@qq.com>
2025-09-02 00:38:11 +0800

    扩展Sanity客户端，新增获取类别及其产品的功能，添加getCategoryBySlug和getProductsByCategorySlug方法。

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -17,8 +17,22 @@ export const getAllProducts = async() => {
   const products = await sanityFetch({query:query})
   return products.data as Product[]
 }
+
+
 export const getAllCategories = async() => {
   const query = `*[_type == "productCategory"]`
   const products = await sanityFetch({query:query})
   return products.data as ProductCategory[]
+}
+
+export const getCategoryBySlug = async (slug: string) => {
+  const query = `*[_type == "productCategory" && slug.current == $slug][0]`
+  const category = await sanityFetch({ query: query, params: { slug } });
+  return category.data as ProductCategory;
+}
+
+export const getProductsByCategorySlug = async (slug: string) => {
+  const query = `*[_type == "product" && references(*[_type == "productCategory" && slug.current == $slug][0]._id)]`
+  const products = await sanityFetch({ query: query, params: { slug } });
+  return products.data as Product[];
 }
